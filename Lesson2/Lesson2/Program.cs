0b0b8457bef299d9ac6a8feb1d22d33179a142ac
Diyorbek Boriboyev <diyorbekzafarugli@gmail.com>
2026-01-20 22:43:41 +0500

    done

---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lesson2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int intValue = int.Parse(Console.ReadLine());
+
+            double pi = 3.14;
+
+            double S = pi * intValue * intValue;
+
+            Console.WriteLine(S);
+        }
+    }
+}
---
+++ Lesson3/Lesson3/Program.cs
@@ -0,0 +1,84 @@
+﻿namespace Lesson3;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //// WHILE LOOP:
+
+        //Random random = new Random();
+        //int randomNumber = random.Next(1, 100);
+        //int i = 0;
+        //int count = 0;
+
+        //while (true)
+        //{
+        //    ++i;
+        //    ++count;
+
+        //    Console.Write($"son toping imkoniatingiz 6 ta, {i} - imkoniyat : ");
+        //    int num = int.Parse(Console.ReadLine());
+
+        //    if (randomNumber == num)
+        //    {
+        //        Console.WriteLine("Ura topdingiz");
+        //        break;
+        //    }
+        //    else if (num > randomNumber)
+        //    {
+        //        Console.WriteLine("Kichikroq kiriting");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Kattaroq son kiriting");
+        //    }
+
+
+        //    if (count == 6)
+        //    {
+        //        Console.WriteLine($"Game over, son = {randomNumber} edi");
+        //        break;
+        //    }
+        //}
+
+
+        /* for loopi */
+
+        //for (int i = 100; i <= 999; i++)
+        //{
+        //    if ((i / 100) % 2 == 1 && (i / 10) % 2 == 1 && (i % 10) % 2 == 1)
+        //    {
+        //        Console.Write($"{i}, {-i} ");
+        //    }
+        //}
+
+        //Console.Write("1-sonni kiriting : ");
+        //int firstValue = int.Parse(Console.ReadLine());
+
+        //Console.Write("2-sonni kiriting : ");
+        //int secondValue = int.Parse(Console.ReadLine());
+
+
+        //for (int i = firstValue; i <= secondValue; i++)
+        //{
+        //    if (i % 3 == 0)
+        //    {
+        //        Console.Write($"{i}, ");
+        //    }
+        //}
+
+        //for (int i = firstValue; i >= secondValue; i--)
+        //{
+        //    if (i % 3 == 0)
+        //    {
+        //        Console.Write($"{i}, ");
+        //    }
+        //}
+
+
+
+
+
+        //Console.WriteLine("Hello, World!");
+    }
+}
